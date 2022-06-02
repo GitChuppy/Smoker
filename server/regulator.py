888ba7690cb6c334d1da7b27793f70e5433deec1
GitChuppy <95688524+GitChuppy@users.noreply.github.com>
2022-06-02 17:09:34 +0200

    Initial commit

---
+++ server/regulator.py
@@ -0,0 +1,45 @@
+    # Created by Bischofberger David
+
+from random import random
+from time import sleep
+# DEBUG, can be removed from fetchSensorValue() when shipping
+
+def getParameters( intern_Tu, intern_Tg ):
+    """returns the PI system parameters of the current system as a tuple: '( Kp, Ti )'."""
+
+    # changeable parameters:
+    intern_Kp  =   (   0.6 * intern_Tg    )   /  (   intern_Tu * intern_Tg    )
+    intern_Ti  =   (   4   * intern_Tu    )   /   intern_Kp
+
+    return (intern_Kp, intern_Ti)
+
+def getResponse( error, error_before, sysParam ):
+    """returns the PI system reaction of the current system as tuple."""
+
+
+    ( intern_Kp, intern_Ti )   =   sysParam     #: unpacking sysParam
+
+    internPResponse =   intern_Kp * error
+    internIResponse =   intern_Ti * (error + error_before)
+
+    return ( internPResponse + internIResponse )
+
+def fetchSensorValue(channel):
+    """expects channel as integer argument, fetches sensor value over SPI from ADC."""
+    return random()
+
+"""main code:"""
+sysParam    = getParameters(1, 5)
+e_b         = 0
+targetValue = 20
+Ks          = 2.2
+
+while True:
+    e = targetValue/fetchSensorValue(channel = 0)
+
+    print(
+        str(
+            Ks * getResponse(e, e_b, sysParam)
+        )
+    )
+    sleep(.05)
---
+++ server/server.py
@@ -0,0 +1,25 @@
+import socket
+
+adapter_addr = '18:ce:94:1a:72:b7'
+
+port = 3  # Normal port for rfcomm? (for now 3)
+print(adapter_addr + '\t:\t' + str(port) +'\n')
+
+buf_size = 1024
+
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.bind((adapter_addr, port))
+s.listen(1)
+try:
+    print('Listening for connection...')
+    client, address = s.accept()
+    print(f'Connected to {address}')
+
+    while True:
+        data = client.recv(buf_size)
+        if data:
+            print(data)
+except Exception as e:
+    print(f'Something went wrong: {e}')
+    client.close()
+    s.close()
